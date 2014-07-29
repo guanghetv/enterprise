e8aa071eced3a8d407440f5f6155740ff0358653
Lsong <song940@gmail.com>
2014-07-29 10:55:38 +0800

    add err handler .

--- libs/engine.js
+++ libs/engine.js
@@ -5,19 +5,26 @@ var taskManager		= require('./core/TaskManager');
 var cacheManager	= require('./core/CacheManager');
 var dataManager		= require('./core/DataManager');
 
-exports.bootstrap = function(config){
+exports.bootstrap = function(config, callback){
 	dataManager.load(function(err, data){
+		if(err)callback(err);
 		cacheManager.storage(data, function(err, res){
+			if(err)callback(err);
 			moduleLoader.load(function(err, modules){
+				if(err)callback(err);
 				moduleLoader.watch(function(err, module){
+					if(err)callback(err);
 					taskManager.register(module);
 				})
 				taskManager.run(modules, res, function(err, name, d){
+					if(err)callback(err);
 					cacheManager.save(name, d, function(err ,r){
+						if(err)callback(err);
 						console.log('[AppEngine] task save :' , r);
 					});
 				});
 			});
 		});
 	});
+	callback();
 };
