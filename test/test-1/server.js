c5158fa7d6dd83e224b348407f2d95ec2f029b42
Lsong <song940@gmail.com>
2014-08-05 10:10:55 +0800

    demo pages .

---
+++ test/test-1/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+
+var app = express();
+
+//simple logger
+app.use(function(req, res, next){
+	console.log("\033[0;32m%s \033[1;33m%s", req.method, req.url);
+	next();
+});
+//serval static file
+var fs = require('fs');
+app.use(function(req, res, next){
+	var url = req.url;
+	if(url == '/'){
+		//default index file.
+		url += 'index.html';
+	}
+	var filename = __dirname + '/public' + url;
+	if(fs.existsSync(filename)){
+		res.send(fs.readFileSync(filename,{ encoding: 'utf-8' }));
+	}else{
+		next();
+	}
+});
+//error handler
+app.use(function(req, res, next){
+	res.status(404).end('404 Not Found .');
+	next();
+});
+
+var server = app.listen(3002, function(){
+	console.log("server is running at %s", server.address().port);
+});
