c34c4b6c1426744f75f050418e522e136f102c96
Yanan Guo <gynsolomon@gmail.com>
2014-08-18 15:13:15 +0800

    [yanan] modify crontab interval

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,14 @@
+var path = require('path');
+
+var dirname = __dirname;
+console.log(dirname);
+var server_path = path.dirname(dirname);
+var enterprise_path = path.dirname(server_path);
+
 module.exports = {
 	development: {
-		db_uri: 'mongodb://localhost/enterprise'
+		db_uri: 'mongodb://localhost/enterprise',
+        server_path: server_path,
+        enterprise_path: enterprise_path
 	}
 };
