faca52f985f1593f298a578f332003995a5062df
Yanan Guo <gynsolomon@gmail.com>
2014-08-25 14:37:29 +0800

    [yanan] enlarge limit for bodyparser

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(function(req, res, next){
 	next();
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true,limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
 //
 mongoose.connect(config.db_url, function(err){
     if(err){
