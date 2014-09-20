a755cbee09cd04e4c58b885fd81d690d0b7a2eac
Yanan Guo <gynsolomon@gmail.com>
2014-09-20 10:16:05 +0800

    [yanan] change config

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
 var path = require('path');
 
-var app_path 		= path.dirname(__dirname);
-var modules_path 	= path.join(app_path, 'modules');
+var app_path = path.dirname(__dirname);
+var modules_path = path.join(app_path, 'modules');
 require('./global');
 
 module.exports = {
-	development: {
-		'modules_path': modules_path,
-		'mothership_url': 'http://localhost:3000',
-		'datapipe_url': 'http://localhost:3002',
+    development: {
+        'modules_path': modules_path,
+        'mothership_url': 'http://localhost:3000',
+        'datapipe_url': 'http://localhost:3002',
         'db_url': 'mongodb://localhost/enterprise'
-	},
-	test: {
+    },
+    test: {
         'modules_path': modules_path,
         'mothership_url': 'http://localhost:10461',
         'datapipe_url': 'http://localhost:3002',
         'db_url': 'mongodb://localhost/enterprise'
-	},
-	production: {
+    },
+    production: {
         'modules_path': modules_path,
-        'mothership_url': 'http://localhost:9000',
+        'mothership_url': 'http://10.122.84.12:9000',
         'datapipe_url': 'http://localhost:3002',
-        'db_url': "mongodb://root:xiaoshu815@10.4.10.99/enterprise"
-	}
+        'db_url': "mongodb://root:xiaoshu815@10.160.68.76/enterprise"
+    }
 };
